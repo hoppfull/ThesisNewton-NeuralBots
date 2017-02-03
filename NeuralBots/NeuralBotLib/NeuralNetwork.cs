d4f8eb3b409c83af7cc28ebc5dbd557759336c2d
bjorn kihlberg <hoppfull@gmail.com>
2017-02-03 14:45:27 +0100

    setup initial infrastructure

---
+++ NeuralBots/NeuralBotLib/NeuralNetwork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotLib {
+    public static class NeuralNetwork {
+        public static int f(int x) {
+            return x * 2;
+        }
+        public static double g(double x) {
+            return x + 1;
+        }
+    }
+}
---
+++ NeuralBots/NeuralBotLibTests/NeuralNetworkTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralBotLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralBotLib.Tests {
+    [TestClass()]
+    public class NeuralNetworkTests {
+        [TestMethod()]
+        public void fTest() {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void gTest() {
+            Assert.Fail();
+        }
+    }
+}
