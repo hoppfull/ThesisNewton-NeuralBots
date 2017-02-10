4450d6f9d3b22484202b3352d4e14d5e679e4c6a
bjorn kihlberg <hoppfull@gmail.com>
2017-02-10 14:23:47 +0100

    added preliminary implementation of genetic datastructure

---
+++ NeuralBots/NeuralBotLib/Genetics.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// http://derekwill.com/2015/03/05/bit-processing-in-c/
+namespace NeuralBotLib {
+    public static class Genetics {
+        private static uint LengthOfGenes(uint[] config) {
+            uint result = 0;
+            for (int i = 1; i < config.Length; i++) result += (config[i - 1] + 1) * config[i];
+            return result;
+        }
+
+        public static int[] Genesis(Func<int> gene, uint[] config) {
+            return new int[LengthOfGenes(config)].Select(_ => gene()).ToArray();
+        }
+
+        public static double[][][] FoldExpression(double[] geneExpression, uint[] config) {
+            double[][][] result = new double[Math.Max(config.Length - 1, 0)][][];
+            int gene = 0;
+            for (int i = 0; i < result.Length; i++) {
+                result[i] = new double[config[i + 1]][];
+                for (int j = 0; j < result[i].Length; j++) {
+                    int nWeights = (int)config[i] + 1;
+                    result[i][j] = geneExpression.Skip(gene).Take(nWeights).ToArray();
+                    gene += nWeights;
+                }
+            }
+            return result;
+        }
+
+        public static double[] ExpressGenes(Func<int, int, double> expressGene, int[] chrA, int[] chrB) {
+            return chrA.Zip(chrB, expressGene).ToArray();
+        }
+
+        public static int[] Replicate(int[] chromosome, Func<int, int> mutation) {
+            return chromosome.Select(mutation).ToArray();
+        }
+
+        #region rand functions
+        public static int fastrand(int seed) {
+            return ((214013 * seed + 2531011) >> 16) & 0x7FFF;
+        }
+
+        public static int hash(int seed) {
+            int x = seed;
+            x = (x ^ 61) ^ (x >> 16);
+            x += x << 3;
+            x ^= x >> 4;
+            x *= 0x27d4eb2d;
+            x ^= x >> 15;
+            return x;
+        }
+
+        public static Func<int> GenHasher(int seed1, int seed2) {
+            int rnd1 = hash(seed1);
+            int rnd2 = hash(seed2);
+            return () => {
+                rnd1 = hash(rnd1);
+                rnd2 = hash(rnd2);
+                return rnd1 * (rnd2 % 2 == 0 ? 1 : -1);
+            };
+        }
+
+        public static Func<int, int> GenMutator(int seed) {
+            int rnd = hash(seed);
+            return x => {
+                rnd = hash(rnd);
+                return x ^ (1 << (rnd % 32));
+            };
+        }
+        #endregion
+    }
+}
---
+++ NeuralBots/NeuralBotLibTests/GeneticsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
+
+using NeuralBotLib;
+
+namespace NeuralBotLibTests {
+    public class GeneticsTests {
+        #region Genetics.Genesis
+        [Theory]
+        [InlineData(new uint[] { }, 0)]
+        [InlineData(new uint[] { 74 }, 0)]
+        [InlineData(new uint[] { 3, 4 }, (3 + 1) * 4)]
+        [InlineData(new uint[] { 2, 3, 8, 7, 4, 6 },
+            (2 + 1) * 3 + (3 + 1) * 8 + (8 + 1) * 7 + (7 + 1) * 4 + (4 + 1) * 6)]
+        public void Genesis_With_Testdata(uint[] config, int length) {
+            Func<int> fa = () => 0;
+            Func<int> fb = () => 1;
+            Func<int> fc = Genetics.GenHasher(234232, 123121);
+            Func<int> fd = Genetics.GenHasher(234232, 123121);
+
+            int[] chromosomeA = Genetics.Genesis(fa, config);
+            int[] chromosomeB = Genetics.Genesis(fb, config);
+            int[] chromosomeC = Genetics.Genesis(fc, config);
+
+            Assert.Equal(length, chromosomeA.Length);
+            Assert.Equal(length, chromosomeB.Length);
+            Assert.Equal(length, chromosomeC.Length);
+
+            Assert.True(chromosomeA.All(gene => gene == 0), "All zero");
+            Assert.True(chromosomeB.All(gene => gene == 1), "All one");
+            Assert.True(chromosomeC.All(gene => gene == fd()), "All random");
+        }
+        #endregion
+
+        #region Genetics.FoldExpression
+        [Theory]
+        [InlineData(new double[] { }, new uint[] { }, 0)]
+        [InlineData(new double[] { }, new uint[] { 3 }, 0)]
+        [InlineData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            new uint[] { 2, 3 }, 1)]
+        [InlineData(new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32 },
+            new uint[] { 3, 3, 1, 2, 4 }, 4)]
+        public void FoldExpression_With_Testdata(double[] expressedGenes, uint[] config, int wsssLength) {
+            double[][][] wsss = Genetics.FoldExpression(expressedGenes, config);
+
+            Assert.Equal(wsssLength, wsss.Length);
+            for (int i = 0; i < wsssLength; i++) {
+                Assert.Equal((int)config[i + 1], wsss[i].Length);
+                for (int j = 0; j < wsss[i].Length; j++)
+                    Assert.Equal((int)config[i] + 1, wsss[i][j].Length);
+            }
+        }
+        #endregion
+    }
+}
